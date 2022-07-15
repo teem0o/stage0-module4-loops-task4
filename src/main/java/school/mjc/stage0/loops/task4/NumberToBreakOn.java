11c8e9f80d6e50a4f43837f1d408da4e270b2170
temo <chxaidzetemo@gmail.com>
2022-07-15 20:13:10 +0400

    fhgfhghgf

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,5 +3,17 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
+        if (toBreakWith>numberToGoUntil){
+            System.out.println("iterating till the end");
+        }
+        for (int i = 0; i < numberToGoUntil; i++){
+            if (++i==toBreakWith){
+                System.out.println(i);
+                break;
+            }else {
+                System.out.println(i);
+            }
+            i--;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,5 +3,11 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
 
+        int s=0;
+        for (int i=0;i<lastInRow;i++){
+
+        }
+
+
     }
 }
